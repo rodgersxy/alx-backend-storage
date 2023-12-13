5939c9118c36abea9f696a9249ca343034351c7a
rodgersxy <rodgersny99@gmaill.com>
2023-12-13 16:02:36 +0000

    fixed error

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,4 @@
--- a SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
+-- a SQL script that lists all bands with Glam rock as their main style, ranked by their longevit
 
-SELECT band_name, COALESCE(split, 2020) - formed as lifespan
-FROM metal_bands
-WHERE style LIKE '%Glam rock%';
+SELECT band_name, COALESCE(split, 2022) - formed as lifespan FROM metal_bands
+WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
