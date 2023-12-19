938a77e549f777810c19f4f1168fa36a39b469c1
rodgersxy <rodgersny99@gmail.com>
2023-12-19 04:46:48 -0800

    update on the file

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+a Python function that returns all students sorted by average score
+"""
+
+
+def top_students(mongo_collection):
+    """
+    Prototype: def top_students(mongo_collection):
+    Returns:
+        all students sorted by average score
+    """
+    return mongo_collection.aggregate([
+        {
+            "$project":
+            {
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {
+            "$sort":
+            {
+                "averageScore": -1
+            }
+        }
+    ])
